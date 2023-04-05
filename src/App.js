fc197eac2a7dfa446bf50241477798b22fcdbc37
Kp <kpericles23@hotmail.com>
2023-04-04 22:49:30 -0400

    test

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import renderer from 'react-test-renderer'
+
 
 function App() {
   return (
@@ -15,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          <h2>Learn React</h2>
         </a>
       </header>
     </div>
